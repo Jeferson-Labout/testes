ded1aaaf09c5de24adc114f12ca8625e50ffceac
Jeferson Labout <70985458+Jeferson-Labout@users.noreply.github.com>
2022-09-08 18:05:03 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ChamadoComponent } from './chamado/chamado.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { LoginComponent } from './login/login.component';
+import { TecnicoComponent } from './tecnico/tecnico.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HomeComponent,
+    HeaderComponent,
+    ChamadoComponent,
+    ClienteComponent,
+    LoginComponent,
+    TecnicoComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
